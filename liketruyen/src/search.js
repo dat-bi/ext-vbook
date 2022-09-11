edcc1e4e05cf46be9ebed4e9db6692cfb0cc79f8
dat-bi <83175246+dat-bi@users.noreply.github.com>
2022-09-11 14:19:20 +0700

    up liketruyen
    d

---
+++ liketruyen/src/search.js
@@ -0,0 +1,26 @@
+function execute(key, page) {
+    if (!page) page = '1';
+    const doc = Http.get("http://liketruyen.net/").params({"s": key, "trang": page}).html();
+
+    var next = doc.select(".pagination").select("li.active + li").text()
+
+    const el = doc.select(".items .item")
+
+    const data = [];
+    for (var i = 0; i < el.size(); i++) {
+        var e = el.get(i);
+        var coverImg = e.select(".image img").first().attr("data-original")
+        if (coverImg.startsWith("//")) {
+            coverImg = "https:" + coverImg
+        }
+        data.push({
+            name: e.select("h3 a").first().text(),
+            link: e.select("h3 a").first().attr("href"),
+            cover: coverImg,
+            description: e.select(".chapter a").first().text(),
+            host: "http://liketruyen.net"
+        })
+    }
+
+    return Response.success(data, next)
+}
