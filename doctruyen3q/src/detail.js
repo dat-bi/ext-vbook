eb70fd9f113df2dbab3a66a37d64c14a1dc7cadb
dat-bi <83175246+dat-bi@users.noreply.github.com>
2022-11-21 17:46:21 +0700

    s
    s

--- doctruyen3q/src/detail.js
+++ doctruyen3q/src/detail.js
@@ -8,7 +8,7 @@ function execute(url) {
         let doc = response.html()
         return Response.success({
             name: doc.select("h1.title-manga").text(),
-            cover: doc.select(".image-info img").first().attr('src'),
+            cover: doc.select(".image-info > img.image-comic").first().attr('data-cfsrc'),
             author: doc.select(".author .detail-info").first().text(),
             description: doc.select(".detail-summary").text(),
             detail: 'Tình trạng : '+doc.select(".status .detail-info").text()+'<br>Nhóm dịch : '+doc.select(".translate-group .detail-info").text()+'<br>Tác giả : '+doc.select(".author .detail-info").text(),
