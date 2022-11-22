fba4f8a0532b68cfaac38989d52eeaafb8b9791f
dat-bi <83175246+dat-bi@users.noreply.github.com>
2022-11-22 10:48:48 +0700

    fix Manga Buddy
    1

---
+++ mangabuddy/src/search.js
@@ -0,0 +1,27 @@
+function execute(key, page) {
+    if (!page) page = '1';
+    let response = fetch('https://mangaforest.com/search',{
+        method : "GET",
+        queries : {
+            q : key,
+            page : page
+        }
+    })
+    if(response.ok){
+        let doc = response.html();
+        let next = doc.select(".paginator").select("a.active + a").text()
+        let el = doc.select(".list .book-detailed-item")
+        let data = [];
+        el.forEach(e =>{
+            data.push({
+                name: e.select("h3 a").first().text(),
+                link: e.select("h3 a").first().attr("href"),
+                cover: e.select("img").first().attr("data-src"),
+                description: e.select(".latest-chapter").first().text(),
+                host: "https://mangaforest.com"
+            })
+        })
+        return Response.success(data, next)
+    }
+    return null
+}
