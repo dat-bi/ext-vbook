35a7dabef201963e229b4c62e98ce178dbbf9a31
dat-bi <83175246+dat-bi@users.noreply.github.com>
2022-11-28 17:56:25 +0700

    fix

--- truyenkk1/src/chap.js
+++ truyenkk1/src/chap.js
@@ -1,12 +1,26 @@
+load("base64.js");
 function execute(url) {
     var doc = Http.get(url+ "/").html()
     var check_content = String(doc.select(".reading")).indexOf("<br>")!=-1
     console.log(check_content)
-    var images = doc.select(".img-center")
-    if(check_content==false){
-        var listImage = [] 
-        images.forEach(image => listImage.push(image.attr("data-echo")))
-        return Response.success(listImage)
+    var id = doc.select("#data").attr("data-id")
+    var redirect = doc.select("#data").attr("data-redirect")
+    if(check_content===false){
+        var doc = Http.post("https://truyenkkz.com/wp-admin/admin-ajax.php")
+        .params({
+            "action" : "user_buy_chap",
+            "post_id" : id,
+            "redirect": redirect,
+        }).html()
+        var genre = ""
+            var el = doc.select("div[id^='kkcode']");
+            for (var i = 0; i < el.size(); i++) {
+                var e = el.get(i);
+                var text = e.text()
+                genre = genre + Base64.decode(text)
+            }
+
+        return Response.success(locRac(genre))
     }
     else{
         var content = doc.select(".reading").html()
@@ -14,4 +28,14 @@ function execute(url) {
 
         return Response.success(content)
     }
+
+}
+function locRac(text){
+    text = text.replace(/{font-size: 1px}/g,"")
+    var st =text.match(/<style>(.*?)<\/style>/g)
+    for(var i=0; i < st.length ; i++){
+        let rac = st[i].replace(/(<style>|<\/style>)/g,"")
+        var text = text.replace(new RegExp("<" + rac + ">(.*?)</" + rac + ">","gm"), "")
+    }
+    return text.replace(/<style>(.*?)<\/style>/g,"")
 }
