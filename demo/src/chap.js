a69364eb815f8e620d03b1c8c7e55b69d6ee59bd
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-01-02 14:59:53 +0700

    .

---
+++ demo/src/chap.js
@@ -0,0 +1,34 @@
+function execute(url) {
+    var response = fetch(url) // GET equest http return Response  //bình thường dùng cái này
+        /*
+    var response = fetch(url, {
+        method: "POST",    // GET, POST, PUT, DELETE, PATCH
+        headers: {
+            "aaa": "xxx",    // 'user-agent': UserAgent.android(), // set chế độ điện thoại
+            "bbb": "yyy"     //'content-type': 'text/html; charset=UTF-8' //type
+        },
+        body: {
+            "aaa": "xxx",
+            "bbb": "yyy"
+        }, //form data thì dùng cái này
+        queries: { 
+            "aaa": "xxx",
+            "bbb": "yyy"
+        } //Query String Parameter thì dùng cái này
+    }) // Full request http với options return Response
+        */
+    if(response.ok){ // Check request success (status >= 200 && status < 300)
+        let doc = response.html() // Trả về response request dạng Document object
+        let doc = response.html(charset) // Trả về response request dạng Document object //Web trung có thể là gbk
+        let text = response.text() // Trả về response request dạng string
+        let text = response.text(charset) // Trả về response request dạng string
+        let json = response.json() // Trả về response request dạng JSONObject
+        let text = json._data.vi
+        var content = ""
+        for(let i = 1; i < text.length - 1; i++){
+            content = content + text[i] + "<br><br>"
+        }
+            return Response.success(content);
+    }
+    return null
+}
--- kanunu8/src/chap.js
+++ kanunu8/src/chap.js
@@ -21,31 +21,33 @@ function execute(url) {
     }
     return null;
 }
-// function execute(url) {
-//     let response = fetch(url);
-//     if (response.ok) {
-//         let doc = response.html('gbk');
-//         let icb = url.split("/")[3];
-//         let icb1 = url.split("/")[4];
-//         if (icb === 'book2'){
-//             var htm = doc.select("body > div:nth-child(1) > table:nth-child(10) > tbody > tr > td:nth-child(2) > p").html();
-//             htm = htm.replace(/&nbsp;/g,'')
+/*
+function execute(url) {
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html('gbk');
+        let icb = url.split("/")[3];
+        let icb1 = url.split("/")[4];
+        if (icb === 'book2'){
+            var htm = doc.select("body > div:nth-child(1) > table:nth-child(10) > tbody > tr > td:nth-child(2) > p").html();
+            htm = htm.replace(/&nbsp;/g,'')
             
-//             var mmm = htm.length;
-//             if(mmm === 0 ){
-//                 var htm = doc.select("#Article > div").html().replace(/&nbsp;/g,'');
-//                 htm = Html.clean(htm, ["p"]).replace(/&nbsp;/g,'').replace(/上一页　 回目录　 下一页/g,'')
-//             }
-//         }
-//         if (icb === 'files'& icb !== 'dushi'){
-//             var htm = doc.select("#content p").html();
-//         }
-//         if(true){
-//             var htm = doc.select("body > div > table:nth-child(5) > tbody > tr > td:nth-child(2) > p").html();
-//             htm = htm.replace(/&nbsp;/g,'')
-//         }
+            var mmm = htm.length;
+            if(mmm === 0 ){
+                var htm = doc.select("#Article > div").html().replace(/&nbsp;/g,'');
+                htm = Html.clean(htm, ["p"]).replace(/&nbsp;/g,'').replace(/上一页　 回目录　 下一页/g,'')
+            }
+        }
+        if (icb === 'files'& icb !== 'dushi'){
+            var htm = doc.select("#content p").html();
+        }
+        if(true){
+            var htm = doc.select("body > div > table:nth-child(5) > tbody > tr > td:nth-child(2) > p").html();
+            htm = htm.replace(/&nbsp;/g,'')
+        }
     
-//         return Response.success(htm);
-//     }
-//     return null;
-// }
+        return Response.success(htm);
+    }
+    return null;
+}
+*/
