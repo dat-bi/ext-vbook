4ce1e6a434df981aa4d4121ccb9071d6d05640c4
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-01-27 15:27:21 +0700

    fix tienhiep2

--- tienhiep/src/chap.js
+++ tienhiep/src/chap.js
@@ -1,8 +1,8 @@
 function execute(url) {
     var doc = Http.get(url).html();
     if (doc) {
-        var txt = doc.select(".chapter-content p").html();
-        return Response.success(txt);
+        var txt = doc.select(".chapter-content").html();
+        return Response.success(txt.replace(/<br>|\\n/g,"<br><br>"));
     }
     return null;
 }
