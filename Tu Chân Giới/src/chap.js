606584b86086d89eefa7354761094125266734db
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-01-27 16:05:48 +0700

    fix tuchangioi

--- Tu Chân Giới/src/chap.js
+++ Tu Chân Giới/src/chap.js
@@ -1,7 +1,7 @@
 function execute(url) {
     var doc = Http.get(url).html();
     if (doc) {
-        var txt = doc.select(".text-truyen p").html();
+        var txt = doc.select("#noidungtruyen").html();
         return Response.success(txt.replace(/<br\s*\/?>|\n/g,"<br><br>"));
     }
     return null;
--- Tu Chân Giới/src/toc.js
+++ Tu Chân Giới/src/toc.js
@@ -2,16 +2,18 @@ function execute(url , page) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        var el = doc.select(".chap-list .ip5 a");
+        var el = doc.select(".span9 > div > div.last-5 > div:nth-child(1) > a").attr("href");
+        let chap = el.match(/chuong-\d+/g)[0].match(/\d+/g)[0]
+        console.log(chap)
         var list = [];
-        for (var i = 0; i < el.size(); i++) {
-            var e = el.get(i);
+        for (var i = 1; i <= chap; i++) {
             list.push({
-                name: e.text(),
-                url: e.attr("href"),
+                name: "Chương " + i,
+                url: url + "/chuong-" + i,
                 host: "https://tuchangioi.net"
             });
-        }return Response.success(list);
+        }
+        return Response.success(list);
     }
     return null;
 }
