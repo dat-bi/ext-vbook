4a3d195c4880e12536f8d151e40f4f912cdb305c
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-02-02 10:12:38 +0700

    fix xuống dòng ntruyen

--- ntruyen/src/chap.js
+++ ntruyen/src/chap.js
@@ -3,7 +3,6 @@ function execute(url) {
     let doc = browser.launch(url, 5000) // Mở trang web với timeout, trả về Document object
     let el1 = doc.select("#chapter-content")
     browser.close()
-    el1 = Html.clean(el1, ["div", "p"])
     return Response.success(el1);
 
 }
