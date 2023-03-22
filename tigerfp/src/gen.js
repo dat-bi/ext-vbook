73fbf4b00f7e92ebabbaafe79709a61c70aded27
Đường đen <79037079+duongden@users.noreply.github.com>
2023-03-22 17:09:36 +0700

    update kệ truyện
    update home.js + gen.js để hiển thị danh sách truyện các mục

--- tigerfp/src/gen.js
+++ tigerfp/src/gen.js
@@ -1,3 +1,23 @@
 function execute(url, page) {
-
+    if(url.slice(-1) === "/")
+        url = url.slice(0, -1)
+	url = url.replace('m.tigerfp.com', 'www.tigerfp.com');
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html();
+        const data = [];
+        //#main > div.news-wrap.wrap-box > div.update-wrap > div > table > tbody
+        let ele1 = doc.select(".all-book-list li")
+        ele1.forEach(e => {
+            data.push({
+                name: e.select(".book-info h4").first().text(),
+                link: "http://www.tigerfp.com" + e.select(".book-info h4 a").attr("href"),
+                cover: e.select(".book-img img").attr("data-original"),
+                description: e.select(".intro").text(),
+                host: "http://www.tigerfp.com"
+            })
+        }); 
+        return Response.success(data)
+    }
+    return null;
 }
--- tigerfp/src/home.js
+++ tigerfp/src/home.js
@@ -1,9 +1,13 @@
 function execute() {
     return Response.success([
-        { title: "玄幻", input: "1", script: "gen.js" },
-        { title: "修真", input: "1", script: "gen.js" },
-        { title: "都市", input: "1", script: "gen.js" },
-        { title: "穿越", input: "1", script: "gen.js" },
-        { title: "网游", input: "1", script: "gen.js" },
+        { title: "玄幻", input: "https://www.tigerfp.com/leibie1/", script: "gen.js" },
+        { title: "修真", input: "https://www.tigerfp.com/leibie2/", script: "gen.js" },
+        { title: "都市", input: "https://www.tigerfp.com/leibie3/", script: "gen.js" },
+        { title: "历史", input: "https://www.tigerfp.com/leibie4/", script: "gen.js" },
+        { title: "网游", input: "https://www.tigerfp.com/leibie5/", script: "gen.js" },
+        { title: "科幻", input: "https://www.tigerfp.com/leibie6/", script: "gen.js" },
+        { title: "女生", input: "https://www.tigerfp.com/leibie7/", script: "gen.js" },
+        { title: "二次元", input: "https://www.tigerfp.com/leibie8/", script: "gen.js" },
+        { title: "其他", input: "https://www.tigerfp.com/leibie9/", script: "gen.js" }
     ]);
 }
