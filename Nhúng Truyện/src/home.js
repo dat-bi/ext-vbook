1a6e770967a95634b581206d22fae1d84cfff988
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-04-12 08:42:57 +0700

    .

--- Nhúng Truyện/src/home.js
+++ Nhúng Truyện/src/home.js
@@ -1,12 +1,62 @@
-function execute(url) {
-    let re = fetch(url).html();
-    let el = re.select(".py-5.px-5.bg-auto.rounded-md.text-sm > ul > li > ul li a")
-    let data = [];
-    el.forEach(e => data.push({
-        input: e.attr("href"),
-        title: e.text().replace("Xếp hạng ",""),
-        script: "gen.js"
-    }))
-    return Response.success(data)
+function execute() {
+    return Response.success([
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qidian-vote",
+            "title": "Đề Cử Khởi Điểm",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qidian-read",
+            "title": "Lượt Đọc Khởi Điểm",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qidian-fan",
+            "title": "Hâm Mộ Khởi Điểm",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qidian-recommend",
+            "title": "Giới Thiệu Khởi Điểm",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qdmm-vote",
+            "title": "Đề Cử Nữ Sinh",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qdmm-read",
+            "title": "Lượt Đọc Nữ Sinh",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qdmm-fan",
+            "title": "Hâm Mộ Nữ Sinh",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/qdmm-recommend",
+            "title": "Giới Thiệu Nữ Sinh",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/zongheng-vote",
+            "title": "Đề Cử Tung Hoành",
+            "script": "gen.js"
+        },
+        {
+            "input": "https://nhungtruyen.com/xep-hang/zongheng-read",
+            "title": "Lượt Đọc Tung Hoành",
+            "script": "gen.js"
+        }
+    ])
 }
-// console.log([...document.querySelectorAll('#app > main > section > div > div > div > div.hidden.lg\:block.lg\:col-span-1.py-5.px-5.bg-auto.rounded-md.text-sm > ul > li ul li a')].map(e => `{title: "${e.innerText}", input: "${e.href.replace(/^(?:\/\/|[^/]+)*/, '')}", script: "gen.js"},`).join('\n'));
+    // let re = fetch(url).html();
+    // let el = re.select(".py-5.px-5.bg-auto.rounded-md.text-sm > ul > li > ul li a")
+    // let data = [];
+    // el.forEach(e => data.push({
+    //     input: e.attr("href"),
+    //     title: e.text().replace("Xếp hạng ",""),
+    //     script: "gen.js"
+    // }))
