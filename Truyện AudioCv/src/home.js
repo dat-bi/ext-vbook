2061602a3bff7ac35502343d51c02b29eac23d6a
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-05-05 16:42:10 +0700

    xoá truyenaudiocv

--- Truyện AudioCv/src/home.js
+++
@@ -1,8 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Mới Cập Nhật", input: "/danh-sach/moi-cap-nhat", script: "gen.js"},
-        {title: "Truyện Full", input: "/danh-sach/truyen-full", script: "gen.js"},
-        {title: "Truyện VIP Free", input: "/danh-sach/free-vip", script: "gen.js"},
-        {title: "Dâm Hiệp", input: "/danh-sach/category/dam-hiep", script: "gen.js"}
-    ]);
-}
