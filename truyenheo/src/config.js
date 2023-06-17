39511b894437f647e4802777fd797ad09f943b84
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-06-17 21:34:00 +0700

    fix tuoi

--- truyenheo/src/config.js
+++ truyenheo/src/config.js
@@ -1,1 +1,7 @@
-const BASE_URL = 'https://truyensextv.me';
+let BASE_URL = 'https://truyensextv.me';
+try {
+    if (CONFIG_URL) {
+        BASE_URL = CONFIG_URL;
+    }
+} catch (error) {
+}
--- tuoinung/src/config.js
+++ tuoinung/src/config.js
@@ -1,1 +1,7 @@
-const BASE_URL = 'https://home.tuoinung.co';
+let BASE_URL = "https://tuoinung.cc";
+try {
+    if (CONFIG_URL) {
+        BASE_URL = CONFIG_URL;
+    }
+} catch (error) {
+}
