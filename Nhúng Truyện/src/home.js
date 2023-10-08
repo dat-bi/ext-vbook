2a8c5bc9cec5eeec2764270dec542fd8a2aa1c19
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-10-08 16:50:29 +0700

    fix nhungtruyen

--- Nhúng Truyện/src/home.js
+++ Nhúng Truyện/src/home.js
@@ -1,57 +1,151 @@
 function execute() {
     return Response.success([
         {
-            "input": "https://nhungtruyen.com/xep-hang/qidian-vote",
-            "title": "Đề Cử Khởi Điểm",
-            "script": "gen.js"
+            "input": "1",
+            "title": "Tiên Hiệp",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/qidian-read",
-            "title": "Lượt Đọc Khởi Điểm",
-            "script": "gen.js"
+            "input": "2",
+            "title": "Huyền Huyễn",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/qidian-fan",
-            "title": "Hâm Mộ Khởi Điểm",
-            "script": "gen.js"
+            "input": "3",
+            "title": "Kỳ Ảo",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/qidian-recommend",
-            "title": "Giới Thiệu Khởi Điểm",
-            "script": "gen.js"
+            "input": "4",
+            "title": "Đô Thị",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/qdmm-vote",
-            "title": "Đề Cử Nữ Sinh",
-            "script": "gen.js"
+            "input": "5",
+            "title": "Khoa Huyễn",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/qdmm-read",
-            "title": "Lượt Đọc Nữ Sinh",
-            "script": "gen.js"
+            "input": "6",
+            "title": "Võng Du",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/qdmm-fan",
-            "title": "Hâm Mộ Nữ Sinh",
-            "script": "gen.js"
+            "input": "7",
+            "title": "Dã Sử",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/qdmm-recommend",
-            "title": "Giới Thiệu Nữ Sinh",
-            "script": "gen.js"
+            "input": "8",
+            "title": "Đồng Nhân",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/zongheng-vote",
-            "title": "Đề Cử Tung Hoành",
-            "script": "gen.js"
+            "input": "9",
+            "title": "Cạnh Kỹ",
+            "script": "gen1.js"
         },
         {
-            "input": "https://nhungtruyen.com/xep-hang/zongheng-read",
-            "title": "Lượt Đọc Tung Hoành",
-            "script": "gen.js"
+            "input": "10",
+            "title": "Huyền Nghi",
+            "script": "gen1.js"
+        },
+        {
+            "input": "11",
+            "title": "Kiếm Hiệp",
+            "script": "gen1.js"
+        },
+        {
+            "input": "111",
+            "title": "Huyền Huyễn Ngôn Tình",
+            "script": "gen1.js"
+        },
+        {
+            "input": "123",
+            "title": "Cổ Đại Ngôn Tình",
+            "script": "gen1.js"
+        },
+        {
+            "input": "161",
+            "title": "Hiện Đại Ngôn Tình",
+            "script": "gen1.js"
+        },
+        {
+            "input": "319",
+            "title": "Huyền Nghi Thần Quái",
+            "script": "gen1.js"
+        },
+        {
+            "input": "320",
+            "title": "Lãng Mạn Thanh Xuân",
+            "script": "gen1.js"
+        },
+        {
+            "input": "321",
+            "title": "Tiên Hiệp Kỳ Duyên",
+            "script": "gen1.js"
+        },
+        {
+            "input": "322",
+            "title": "Khoa Huyễn Không Gian",
+            "script": "gen1.js"
         }
-    ])
+    ]);
 }
+// function execute() {
+//     return Response.success([
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qidian-vote",
+//             "title": "Đề Cử Khởi Điểm",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qidian-read",
+//             "title": "Lượt Đọc Khởi Điểm",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qidian-fan",
+//             "title": "Hâm Mộ Khởi Điểm",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qidian-recommend",
+//             "title": "Giới Thiệu Khởi Điểm",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qdmm-vote",
+//             "title": "Đề Cử Nữ Sinh",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qdmm-read",
+//             "title": "Lượt Đọc Nữ Sinh",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qdmm-fan",
+//             "title": "Hâm Mộ Nữ Sinh",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/qdmm-recommend",
+//             "title": "Giới Thiệu Nữ Sinh",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/zongheng-vote",
+//             "title": "Đề Cử Tung Hoành",
+//             "script": "gen.js"
+//         },
+//         {
+//             "input": "https://nhungtruyen.com/xep-hang/zongheng-read",
+//             "title": "Lượt Đọc Tung Hoành",
+//             "script": "gen.js"
+//         }
+//     ])
+// }
     // let re = fetch(url).html();
     // let el = re.select(".py-5.px-5.bg-auto.rounded-md.text-sm > ul > li > ul li a")
     // let data = [];
