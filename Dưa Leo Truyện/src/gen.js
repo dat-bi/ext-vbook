8de70417bc72436fa434df9b9e64d4343875ac9b
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-11-13 10:42:53 +0700

    .

--- Dưa Leo Truyện/src/gen.js
+++ Dưa Leo Truyện/src/gen.js
@@ -1,15 +1,15 @@
 load('config.js');
 function execute(url, page) {
     if (!page) page = 1;
-    let response = fetch(BASE_URL + url + "?page=" + page);
+    let response = fetch(BASE_URL + url + ".html?page=" + page);
     if (response.ok) {
         let doc = response.html();
         let next = page + 1;
         let data = [];
-        doc.select("#kt_app_content_container > div > div.col-lg-8 > div > div > div > div").forEach(e => {
+        doc.select("div.box_list .li_truyen").forEach(e => {
             let coverImg = e.select("img").first().attr("src");
             data.push({
-                name: e.select("div.mh-60px>div a").first().text(),
+                name: e.select(".name").first().text(),
                 link: e.select("a").first().attr("href"),
                 cover: coverImg,
                 host: BASE_URL
