98816daf86e10c6bd5d69f0cd01814372a06fecc
dat-bi <83175246+dat-bi@users.noreply.github.com>
2024-02-20 11:30:34 +0700

    .

--- hacamchicac/src/chap.js
+++ hacamchicac/src/chap.js
@@ -6,10 +6,13 @@ function execute(url) {
         var doc = response.html()
         let data = [];
         doc.select(".inner-entry-content p img").forEach(e => {
-            let img = e.attr("src").replace("/wp-content/uploads/2022/09/15/credit509e62cbedaafe86.jpg","").replace("https://hacamchicac.com/tt.jpg","");
-            if(img){
-                data.push(img);
+            let img = e.attr("src").replace("https://i.imgur.com/XTuTfE4.jpg","").replace("https://hacamchicac.com/tt.jpg","");
+            if (!img.includes("https:") && img != ""){
+                img  = "https://hacamchicac.com" +img
             }
+            if(img != "")
+                data.push(img);
+            
         });
         return Response.success(data);
     }
