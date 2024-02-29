eba4ca188cdb64290aeecc30f944a7b61e12040a
dat-bi <83175246+dat-bi@users.noreply.github.com>
2024-02-29 10:58:32 +0700

    fix truyenfullcom

--- truyenfullCom/src/toc.js
+++ truyenfullCom/src/toc.js
@@ -1,5 +1,5 @@
 function execute(url) {
-    let url1 = fetch(url).html().select("#list-chapter ul > li:nth-child(1) a").first().attr("href").replace(/chuong.*?html/g, "").replace("https://truyenfull.com", "https://truyenfull1.com")
+    let url1 = fetch(url).html().select("#list-chapter ul > li:nth-child(1) a").first().attr("href").replace(/chuong.*?html/g, "").replace(/arc-.*?html/g, "").replace("https://truyenfull.com", "https://truyenfull1.com")
     var truyenId = url.replace(/\.com\/.*?\./g, "").match(/\d+/g)[0];
     let response = fetch("https://truyenfull.com/api/chapters/" + truyenId)
     if (response.ok) {
