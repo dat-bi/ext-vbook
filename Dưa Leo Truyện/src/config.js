6ecf82619f8468d8c75513f440f37571a3602d1f
dat-bi <83175246+dat-bi@users.noreply.github.com>
2024-03-18 08:47:16 +0700

    fix men18, dule

--- Dưa Leo Truyện/src/config.js
+++ Dưa Leo Truyện/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = 'https://dualeotruyennet.com'
+let BASE_URL = 'https://dualeotruyenza.com/'
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
--- Men18/src/config.js
+++ Men18/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://hen18.biz";
+let BASE_URL = "https://hen18.vip";
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
