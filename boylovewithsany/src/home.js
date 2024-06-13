edb153d99e2a90fe3c2071285d88f53678cad0b7
dat-bi <83175246+dat-bi@users.noreply.github.com>
2024-06-13 20:31:46 +0700

    .

--- boylovewithsany/src/home.js
+++ boylovewithsany/src/home.js
@@ -1,7 +1,7 @@
 function execute() {
     return Response.success([
         { title: "Manhwa", input: "/manhwa/", script: "gen.js" },
-        { title: "Manga List ", input: "/manhwa/", script: "gen.js" },
+        { title: "Manga List ", input: "/manga-list/", script: "gen.js" },
     ]);
 }
 // console.log([...document.querySelectorAll('body > div.logo2 > div:nth-child(36) a')].map(e => `{title: "${e.innerText}", input: "${e.href.replace(/^(?:\/\/|[^/]+)*/, '')}", script: "gen.js"},`).join('\n'));
