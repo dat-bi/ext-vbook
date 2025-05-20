011d7644d701df2d70c3595e1f4631c5b4b0beda
dat-bi <83175246+dat-bi@users.noreply.github.com>
2025-05-20 10:25:30 +0700

    x

--- qidian-vp/src/chap.js
+++ qidian-vp/src/chap.js
@@ -3,8 +3,8 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        htm.select("h2").remove();
         let htm = doc.select("#chapter-content");
+        htm.select("h2").remove();
         return Response.success(htm);
     }
     return null;
