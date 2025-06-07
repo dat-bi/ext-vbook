0426601930318253038c8aeeccdaede8fa23f94e
dat-bi <83175246+dat-bi@users.noreply.github.com>
2025-06-07 10:58:43 +0700

    .
    .

--- ntruyen/src/chap.js
+++ ntruyen/src/chap.js
@@ -7,7 +7,16 @@ function execute(url) {
         if (data.html().length < 100) {
             data = doc.select("#chapter-content")
         }
+        data.select(".slide").remove()
+        data = cleanHtml(data.html());
         return Response.success(data);
     }
     return null;
+}
+function cleanHtml(htm) {
+  return htm
+            .replace(/·/g, '')
+            .replace(/&nbsp;/gi, '')
+            .replace(/<p>\s*(?:&nbsp;)?\s*<\/p>/gi, '')
+            .replace(/<\/p>\s*<p[^>]*>/gi, '<br>')
 }
