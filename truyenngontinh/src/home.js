9f3d41d8b150194667c1b531423a1d1db38a15d0
dat-bi <83175246+dat-bi@users.noreply.github.com>
2025-07-01 08:54:40 +0700

    fix qidian-vp

--- truyenngontinh/src/home.js
+++
@@ -1,7 +0,0 @@
-function execute() {
-    return Response.success([
-        { title: "Mới cập nhật", input: "/", script: "gen.js" },
-        { title: "Truyện dài", input: "/truyen-dai-tieu-thuyet/", script: "gen.js" },
-        { title: "Truyện ngắn", input: "/truyen-ngan/", script: "gen.js" },
-    ]);
-}
