41aca1483942a86664c546afeca49323f498a02a
dat-bi <83175246+dat-bi@users.noreply.github.com>
2025-07-21 22:08:47 +0700

    xoá

--- tienhiep/src/chap.js
+++
@@ -1,8 +0,0 @@
-function execute(url) {
-    var doc = Http.get(url).html();
-    if (doc) {
-        var txt = doc.select(".chapter-content").html();
-        return Response.success(txt.replace(/<br>|\\n/g,"<br><br>"));
-    }
-    return null;
-}
