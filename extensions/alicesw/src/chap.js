599c4701f26ddf192cc223912586ddcce1f6f5f0
dat-bi <83175246+dat-bi@users.noreply.github.com>
2026-05-03 20:41:06 +0700

    .

--- extensions/alicesw/src/chap.js
+++ extensions/alicesw/src/chap.js
@@ -1,8 +1,14 @@
 function execute(url) {
-    var browser = Engine.newBrowser(); 
-    browser.setUserAgent(UserAgent.android()); // Tùy chỉnh user agent
-    var doc = browser.launch(url, 5000);
+    var browser = Engine.newBrowser();
+    browser.setUserAgent(UserAgent.android());
+
+    browser.launchAsync(url);
+
+    browser.waitUrl([".*?alicesw.com/home/chapter/info.*?"], 4000);
+
+    let doc = browser.html(3000);
     browser.close();
+
     var htm = doc.select(".content_txt").html();
     return Response.success(htm);
 }
